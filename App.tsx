6f06b03a51034a22bc463a868b5101a1e5a6f3ee
sLiMFly <slimflycode@gmail.com>
2025-02-25 19:20:55 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FAB from './components/FAB';
 
 export default function App() {
+
+  const [count, setCount] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>  {count}  </Text>
+
+      {/* <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}>
+        <Text style={{ color: 'white', fontSize: 20 }}>+1</Text>
+      </Pressable> */}
+
+      <FAB label='+1'
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+        position='right'
+      />
+
+      <FAB label='Reset'
+        onPress={() => setCount(0)}
+        position='left'
+      />
+
+
       <StatusBar style="auto" />
-    </View>
+    </View >
   );
 }
 
@@ -17,4 +42,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100',
+  },
+
+
 });
